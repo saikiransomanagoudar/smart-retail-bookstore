16eb715eaf9b8502f589e3f74eec66b63a33e46c
Shubham Gupta <62328086+guptashubham95a@users.noreply.github.com>
2024-10-15 20:01:49 -0500

    added signup and login apis with jwt token

--- backend_node/app.js
+++ backend_node/app.js
@@ -5,7 +5,7 @@ require("./conn/conn");
 const user = require("./routes/user");
 
 app.use(express.json());
-app.use("ap1/v1", user);
+app.use("/api/v1", user);
 app.listen(process.env.PORT, () => {
   console.log(`Server started at ${process.env.PORT}`);
 });
