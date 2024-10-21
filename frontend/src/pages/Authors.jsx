0129b7cf656517c0ef80aed8661475be907d6374
Saranya <salamuri1@hawk.iit.edu>
2024-10-22 00:59:44 +0530

    Fav Genre and fav Author pages

---
+++ frontend/src/pages/Authors.jsx
@@ -0,0 +1,12 @@
+import AuthorsCards from "../components/Authors/AuthorsCards";
+
+const Authors = () => {
+    return (
+      <>
+      <AuthorsCards />
+      </>
+    )
+}
+
+
+export default Authors;
