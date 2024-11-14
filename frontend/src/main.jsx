591e1ddc7a44f1febafa096920ea1b611ff9aa37
Shubham Gupta <sgupta101@hawk.iit.edu>
2024-11-13 20:20:18 -0600

    Adding Redux for books state

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,15 +4,21 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
-const PUBLISHABLE_KEY = "pk_test_ZmFtb3VzLXNhbG1vbi00LmNsZXJrLmFjY291bnRzLmRldiQ";
+const PUBLISHABLE_KEY =
+  "pk_test_ZmFtb3VzLXNhbG1vbi00LmNsZXJrLmFjY291bnRzLmRldiQ";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <Router>
-        <App />
-      </Router>
+      <Provider store={store}>
+        {/* Wrap your app with Provider */}
+        <Router>
+          <App />
+        </Router>
+      </Provider>
     </ClerkProvider>
   </React.StrictMode>
 );
