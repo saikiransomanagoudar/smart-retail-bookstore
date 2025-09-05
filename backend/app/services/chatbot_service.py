84256f28cd8cec1ad28a2837cfb2cd6accd9d776
saikiransomanagoudar <saikiransomanagoudar@gmail.com>
2025-09-05 08:57:23 -0500

    Fix langgraph API - use set_entry_point and set_finish_point instead of reserved nodes

--- backend/app/services/chatbot_service.py
+++ backend/app/services/chatbot_service.py
@@ -83,18 +83,20 @@ class ChatbotService:
         self.state_graph.add_node("order_query_agent", self.order_query_agent)
         self.state_graph.add_node("fraudulent_transaction_agent", self.fraudulent_transaction_agent)
 
-        # Define edges
-        self.state_graph.add_edge("__start__", "user_proxy_agent")
+        # Set entry point and define edges
+        self.state_graph.set_entry_point("user_proxy_agent")
         self.state_graph.add_edge("user_proxy_agent", "operator_agent")
         self.state_graph.add_edge("operator_agent", "recommendation_agent")
         self.state_graph.add_edge("operator_agent", "order_placement_agent")
         self.state_graph.add_edge("operator_agent", "order_query_agent")
         self.state_graph.add_edge("operator_agent", "fraudulent_transaction_agent")
-        self.state_graph.add_edge("operator_agent", "__end__")
-        self.state_graph.add_edge("recommendation_agent", "__end__")
-        self.state_graph.add_edge("order_placement_agent", "__end__")
-        self.state_graph.add_edge("order_query_agent", "__end__")
-        self.state_graph.add_edge("fraudulent_transaction_agent", "__end__")
+        
+        # Set finish points
+        self.state_graph.set_finish_point("operator_agent")
+        self.state_graph.set_finish_point("recommendation_agent")
+        self.state_graph.set_finish_point("order_placement_agent")
+        self.state_graph.set_finish_point("order_query_agent")
+        self.state_graph.set_finish_point("fraudulent_transaction_agent")
 
         # Compile the graph
         self.compiled_graph = self.state_graph.compile()
